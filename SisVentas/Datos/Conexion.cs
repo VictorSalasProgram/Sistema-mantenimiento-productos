2bd10377c2f78e57750fb941ba237880ac29ce5a
Victor <victorsalasok@gmail.com>
2023-09-12 10:22:27 -0300

    Agregar archivos de proyecto.

---
+++ SisVentas/Datos/Conexion.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace SisVentas.Datos
+{
+    public class Conexion
+    {
+        private string Base;
+        private string Servidor;
+        private string Puerto;
+        private string Usuario;
+        private string Clave;
+        private static Conexion Con = null;
+
+        private Conexion()
+        {
+            this.Base = "BD_ventas";
+            this.Servidor = "localhost";
+            this.Puerto = "3306";
+            this.Usuario = "root";
+            this.Clave = "123456";
+        }
+        public MySqlConnection CrearConexion()
+        {
+            MySqlConnection Cadena = new MySqlConnection();
+            try
+            {
+                Cadena.ConnectionString = "datasource" + this.Servidor +
+                                        ";port=" + this.Puerto +
+                                        ";username=" + this.Usuario +
+                                        ";password=" + this.Clave +
+                                        ";databse=" + this.Base;
+
+            }
+            catch (Exception ex) 
+            {
+                Cadena = null;
+                throw ex;
+            }
+            return Cadena;
+            
+
+        }
+        public static Conexion getInstancia()
+        {
+            if ( Con == null )
+            {
+                Con = new Conexion();
+            }
+            return Con;
+        }
+    }
+}
